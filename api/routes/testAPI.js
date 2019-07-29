585904d916c7fe872ad7df640a3fdf0404ce0578
Mickey <smithmick14@gmail.com>
2019-07-29 07:37:16 -0500

    Adding frontend and backend directories

---
+++ api/routes/testAPI.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('API is working properly');
+});
+
+router.post('/', function(req, res, next) {
+
+    var firstName = req.body.first;
+    var lastName = req.body.last;
+    var email = req.body.email;
+
+    var responseString = 'First: ' + firstName +
+                         '\nLast:  ' + lastName +
+                         '\nEmail: ' + email;
+
+    // This is where we would send this data to the MongoDB
+
+    res.send(responseString);
+});
+
+module.exports = router;
